414b989030b5c3d0ea3c46e58d9e4549fac607a7
anuragdesai67 <anuragdesai67@gmail.com>
2018-03-09 12:55:28 -0600

    latest after cnn hyperparameters

--- lab2.py
+++ lab2.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Mar  5 18:06:04 2018
-
 @author: anurag
 """
 # Importing libraries
 
-import numpy
+import numpy as np
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense,Dropout, Activation, Flatten, Input, Convolution2D, MaxPooling2D
@@ -18,6 +17,7 @@ from keras import backend as K
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
 
 
 
@@ -46,52 +46,53 @@ def encoding_train_test(train_y,test_y):
 def create_model(embedding_dim,act_fun):
     model = Sequential()
     model.add(Dense(16, input_dim=embedding_dim, activation=act_fun))
-    model.add(Dense(10, activation=act_fun))
+    #model.add(Dense(10, activation=act_fun))
     model.add(Dense(10, activation='softmax'))
     return model
 
 # Compile model
 def compile_model(model, error_function):
-    
-    model.compile(loss=error_function, optimizer='adam', metrics=['accuracy'])
+    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    adam = optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(loss=error_function, optimizer=adam, metrics=['accuracy'])
     return model
 
 # Training the model
 def train_model(model,num_epochs,b_size,train_x,encoding_train_y):
-    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+    import time
+    start = time.clock()
     model.fit(train_x, encoding_train_y, validation_split = 0.2,epochs=num_epochs, batch_size=b_size, callbacks=[early_stopping])
-
+    print (time.clock() - start)
 # Evaluate the model
 def evaluate_model(model,test_x,encoding_test_y):
     scores = model.evaluate(test_x, encoding_test_y)
     return scores
 
-def build_cnn(train_x,encoding_train_y,test_x,encoding_test_y):
+def build_cnn(train_x,encoding_train_y,test_x,encoding_test_y,dropout_prob1,dropout_prob2,num_epochs,number_of_filters,filter_size):
     train_x = train_x.reshape(-1,8,8,1)
     test_x = test_x.reshape(-1,8,8,1)
+      
     
-    # Model Hyperparameters
-    # following parameters are just examples, you can use different values according to their meaning
-    sequence_length = 50 # you need to change this value to adapt to your trainning data set because you have different trainning set after you do data augmentation
-    num_epochs = 25       
-    filter_sizes = (2, 3, 4)
-    num_filters = 100
-    dropout_prob = (0.5, 0.5)
-    hidden_dims = 100
     print (train_x.shape)
     model = Sequential()
-    model.add(Convolution2D(32, 3, 3, input_shape=(8,8,1), activation='relu',border_mode = 'valid')) # use this border_mode b/c not using Theano
-    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Convolution2D(number_of_filters, filter_size,filter_size, input_shape=(8,8,1), activation='relu',border_mode = 'valid')) # use this border_mode b/c not using Theano
+    model.add(Convolution2D(number_of_filters, filter_size, filter_size, border_mode='same'))
     model.add(MaxPooling2D(pool_size=(2,2), dim_ordering='th')) # channel dimension/depth is dim 1 to match 32 x 3 x 3
-    model.add(Dropout(0.3))
+    model.add(Dropout(dropout_prob1))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(dropout_prob2))
     model.add(Dense(10, activation='softmax'))
+    
+
     
     
     model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
+    import time
+    start = time.clock()
     model.fit(train_x,encoding_train_y, batch_size=128, nb_epoch=num_epochs,validation_split=0.2)
+    print (time.clock() - start)
     scores = model.evaluate(test_x, encoding_test_y, verbose=0)
     print("\nAccuracy: %.2f%%" % (scores[1]*100))
     print("\n")
@@ -102,71 +103,97 @@ def build_cnn(train_x,encoding_train_y,test_x,encoding_test_y):
     y_pred = model.predict_classes(test_x)
     print(classification_report(test_y, y_pred))
     print(confusion_matrix(test_y, y_pred))
+    
+def get_normed_mean_cov(X):
+    X_std = StandardScaler().fit_transform(X)
+    X_mean = np.mean(X_std, axis=0)
+    
+    ## Automatic:
+    #X_cov = np.cov(X_std.T)
+    
+    # Manual:
+    X_cov = (X_std - X_mean).T.dot((X_std - X_mean)) / (X_std.shape[0]-1)
+    
+    return X_std, X_mean, X_cov
 
     
 # 1-of-c encoding output encoding
 train_x,train_y = import_train_data("optdigits.tra")
 test_x,test_y = import_test_data("optdigits.tes")
 encoding_train_y,encoding_test_y = encoding_train_test(train_y,test_y)
-
+#X_std, X_mean, X_cov = get_normed_mean_cov(X_training)
+# =============================================================================
+# train_x, _, _ = get_normed_mean_cov(train_x)
+# test_x, _, _ = get_normed_mean_cov(test_x)
+# =============================================================================
 
 embedding_dim = 64
 # Training parameters
 batch_size = 32
-num_epochs = 30      # you need to change this value to set how many epochs you want model fit your trainning data before evaluate performance on test data
+num_epochs = 30     
 model = create_model(embedding_dim,K.relu)
 
-print("Sum of Squares error---------------------------------------------------------------------------")
-# Sum of Squares Error
-model = compile_model(model,losses.mean_squared_error)
-train_model(model,num_epochs,batch_size,train_x,encoding_train_y)
-scores = evaluate_model(model,test_x,encoding_test_y)
-print("\nAccuracy: %.2f%%" % (scores[1]*100))
-print("\n")
-y_pred = model.predict_classes(train_x)
-print(classification_report(train_y, y_pred))
-print(confusion_matrix(train_y, y_pred))
-y_pred = model.predict_classes(test_x)
-print(classification_report(test_y, y_pred))
-print(confusion_matrix(test_y, y_pred))
-
-print("Cross Entropy Error Function---------------------------------------------------------------------------")
-# Cross Entropy Error Function
-model = compile_model(model,losses.categorical_crossentropy)
-train_model(model,num_epochs,batch_size,train_x,encoding_train_y)
-scores = evaluate_model(model,test_x,encoding_test_y)
-print("\nAccuracy: %.2f%%" % (scores[1]*100))
-print("\n")
-
-y_pred = model.predict_classes(train_x)
-print(classification_report(train_y, y_pred))
-print(confusion_matrix(train_y, y_pred))
-
-y_pred = model.predict_classes(test_x)
-
-print(classification_report(test_y, y_pred))
-print(confusion_matrix(test_y, y_pred))
-
-print("tanh hidden unit---------------------------------------------------------------------------")
-
-#tanh hidden unit
-model = create_model(embedding_dim,K.tanh)
-model = compile_model(model,losses.categorical_crossentropy)
-train_model(model,num_epochs,batch_size,train_x,encoding_train_y)
-scores = evaluate_model(model,test_x,encoding_test_y)
-print("\nAccuracy: %.2f%%" % (scores[1]*100))
-print("\n")
-
-y_pred = model.predict_classes(train_x)
-print(classification_report(train_y, y_pred))
-print(confusion_matrix(train_y, y_pred))
-y_pred = model.predict_classes(test_x)
-print(classification_report(test_y, y_pred))
-print(confusion_matrix(test_y, y_pred))
+# =============================================================================
+# print("Sum of Squares error---------------------------------------------------------------------------")
+# # Sum of Squares Error
+# model = compile_model(model,losses.mean_squared_error)
+# train_model(model,num_epochs,batch_size,train_x,encoding_train_y)
+# scores = evaluate_model(model,test_x,encoding_test_y)
+# print("\nOverall ClassificationAccuracy: %.2f%%" % (scores[1]*100))
+# print("\n")
+# print("TRAINING DATA---------------------------------------------------------------------------")
+# y_pred = model.predict_classes(train_x)
+# print(classification_report(train_y, y_pred))
+# print(confusion_matrix(train_y, y_pred))
+# print("TESTING DATA---------------------------------------------------------------------------")
+# y_pred = model.predict_classes(test_x)
+# print(classification_report(test_y, y_pred))
+# print(confusion_matrix(test_y, y_pred))
+# 
+# print("Cross Entropy Error Function---------------------------------------------------------------------------")
+# # Cross Entropy Error Function
+# model = compile_model(model,losses.categorical_crossentropy)
+# train_model(model,num_epochs,batch_size,train_x,encoding_train_y)
+# scores = evaluate_model(model,test_x,encoding_test_y)
+# print("\nAccuracy: %.2f%%" % (scores[1]*100))
+# print("\n")
+# print("TRAINING DATA---------------------------------------------------------------------------")
+# y_pred = model.predict_classes(train_x)
+# print(classification_report(train_y, y_pred))
+# print("\n")
+# print("\n")
+# print(confusion_matrix(train_y, y_pred))
+# print("TESTING DATA---------------------------------------------------------------------------")
+# y_pred = model.predict_classes(test_x)
+# print(classification_report(test_y, y_pred))
+# print(confusion_matrix(test_y, y_pred))
+# 
+# 
+# 
+# print("tanh hidden unit---------------------------------------------------------------------------")
+# 
+# #tanh hidden unit
+# model = create_model(embedding_dim,K.tanh)
+# model = compile_model(model,losses.categorical_crossentropy)
+# train_model(model,num_epochs,batch_size,train_x,encoding_train_y)
+# scores = evaluate_model(model,test_x,encoding_test_y)
+# print("\nAccuracy: %.2f%%" % (scores[1]*100))
+# print("\n")
+# 
+# y_pred = model.predict_classes(train_x)
+# print(classification_report(train_y, y_pred))
+# print(confusion_matrix(train_y, y_pred))
+# y_pred = model.predict_classes(test_x)
+# print(classification_report(test_y, y_pred))
+# print(confusion_matrix(test_y, y_pred))
+# =============================================================================
 
 
 print("CNN---------------------------------------------------------------------------")
-
 # CNN
-build_cnn(train_x,encoding_train_y,test_x,encoding_test_y)
-            
+# Model Hyperparameters
+num_epochs = 25       
+filter_sizes = (2, 3, 4)
+number_of_filters = 32
+dropout_prob = (0.5, 0.5, 0.3)
+build_cnn(train_x,encoding_train_y,test_x,encoding_test_y,dropout_prob[2],dropout_prob[1],num_epochs,number_of_filters,filter_sizes[2])
